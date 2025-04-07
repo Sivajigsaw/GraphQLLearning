e66e6479524a886ef6a77230803275f3468be542
Sivajigsaw <sivajigsaw@gmail.com>
2025-04-07 19:53:09 +0200

    Add Mutation tests

--- src/test/java/com/siva/graphql/EmployeeControllerTest.java
+++ src/test/java/com/siva/graphql/EmployeeControllerTest.java
@@ -1,17 +1,24 @@
 package com.siva.graphql;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.siva.graphql.controller.EmployeeController;
 import com.siva.graphql.data.Employee;
 import com.siva.graphql.data.providers.AddressDataProvider;
 import com.siva.graphql.data.providers.DepartmentDataProvider;
 import com.siva.graphql.data.providers.EmployeeDataProvider;
 import com.siva.graphql.service.EmployeeService;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.GraphQlTest;
 import org.springframework.graphql.test.tester.GraphQlTester;
 import org.springframework.test.context.ContextConfiguration;
+
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @GraphQlTest(EmployeeController.class)
 @ContextConfiguration(classes= {EmployeeController.class, EmployeeService.class, EmployeeDataProvider.class, AddressDataProvider.class, DepartmentDataProvider.class})
@@ -19,12 +26,29 @@ public class EmployeeControllerTest {
     @Autowired
     private GraphQlTester graphQlTester;
 
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
     @Test
     void testEmployeeById(){
         // Document Name should match the name in the graphql-test file i.e. getEmployeeDetails.graphql
         Employee employee = graphQlTester.documentName("getEmployeeDetails")
-                .variable("id", "100")
+                .variable("id", "100").operationName("getEmployeeDetails")
                 .execute().path("employeeById").entity(Employee.class).get();
-        Assertions.assertEquals("Jeeva", employee.getName());
+        assertEquals("Jeeva", employee.getName());
     }
+
+    @Test
+    void testCreateAddress() throws JsonProcessingException {
+        // Operation name should match the Operation given in this case mutation createAddress($addressInput: AddressInput) createAddress
+        AddressDataProvider.AddressInput addressInput = new AddressDataProvider.AddressInput("Address Line1", "Address Line2", "zip", "city", "state");
+        Map<String, Object> addressInputMap = objectMapper
+                .convertValue(addressInput, new TypeReference<Map<String, Object>>() {});
+        AddressDataProvider.Address address = graphQlTester.documentName("getEmployeeDetails")
+                .variable("addressInput", addressInputMap).operationName("createAddress")
+                .execute().path("addAddress").entity(AddressDataProvider.Address.class).get();
+        assertEquals("Address Line1", address.addressLine1());
+        assertNotNull(address.id());
+    }
+
+
 }
